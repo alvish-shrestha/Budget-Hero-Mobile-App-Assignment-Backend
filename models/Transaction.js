5b733606a4da4ff5a3b437d3fc39ff9c12a79d4d
alvi-sh <alvishstha97@gmail.com>
2025-07-11 21:58:43 +0545

    user done + transaction started

---
+++ models/Transaction.js
@@ -0,0 +1,11 @@
+// const mongoose = require('mongoose')
+//
+// const TransactionSchema = new mongoose.Schema(
+//     {
+//         type: {
+//             type: String,
+//             enum: ["income", "expense"],
+//             required: true,
+//         }
+//     }
+// )
